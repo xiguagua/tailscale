a13d5ee08a125771289ee3437a7e8df972f42faa
Irbe Krumina <irbe@tailscale.com>
2024-07-11 06:00:36 +0100

    scripts: install on kind
    Signed-off-by: Irbe Krumina <irbe@tailscale.com>

--- scripts/kubetests/setup.sh
+++ scripts/kubetests/setup.sh
@@ -1,18 +1,34 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-set -eu
+set -eux
 
-REPO="$REPO"
 OAUTH_CLIENT_ID="$OAUTH_CLIENT_ID"
 OAUTH_CLIENT_SECRET="$OAUTH_CLIENT_SECRET"
+KIND=${KIND:="false"}
 
-TAG=$(eval `./tool/go run ./cmd/mkversion`)
+if [[ "$KIND" == "true" ]]; then
+  REPO="tailscale-for-kind"
+fi
+
+REPO="$REPO"
+
+eval `./tool/go run ./cmd/mkversion`
+
+args=(TAGS="${VERSION_SHORT}")
 
 make kube-generate-all # ensure things are up to date
 
-# TODO: add a way to build images locally and load into a kind cluster
-REPO="${REPO}/proxy" TAGS="${TAG}" make publishdevimage
-REPO="${REPO}/operator" TAGS="${TAG}" make publishdevoperator
+if [[ "$KIND" == "true" ]]; then
+args+=" PLATFORM=local"
+fi
+
+make ${args[@]} REPO="${REPO}/proxy" publishdevimage
+make ${args[@]} REPO="${REPO}/operator" publishdevoperator
+
+if [[ "$KIND" == "true" ]]; then
+  kind load docker-image  "${REPO}/operator:${VERSION_SHORT}"
+  kind load docker-image  "${REPO}/proxy:${VERSION_SHORT}"
+fi
 
 kubectl apply -f ./cmd/k8s-operator/deploy/crds/
 
@@ -22,15 +38,22 @@ helm upgrade \
   --namespace tailscale \
   --create-namespace \
   --set operator.image.repo="${REPO}/operator" \
-  --set operator.image.tag="${TAG} \
+  --set operator.image.tag="${VERSION_SHORT} \
+  --set opertor.image.pullPolicy="IfNotPresent" \
   --set proxy.image.repo="${REPO}/proxy \
-  --set proxy.image.tag="${TAG}" \
+  --set proxy.image.tag="${VERSION_SHORT}" \
   --set installCRDs=false \
   --set-string apiServerProxyConfig.mode="true" \
   --set oauth.clientId="${OAUTH_CLIENT_ID}" \
   --set oauth.clientSecret="${OAUTH_CLIENT_SECRET}" \
   --set operatorConfig.logging=debug \
+  --wait
 
-## ingress-nginx is used in tests
-
+# ingress-nginx is used in tests.
+# Note that this command CANNOT be ran with --wait as the Service will never
+# become ready (load balancer cannot be provisioned on kind).
 helm upgrade --install ingress ingress-nginx/ingress-nginx
+
+# TODO: either wait for the ingress-controller Pod to become ready or do
+# something else to wait for the parts we care about to be ready.
+
